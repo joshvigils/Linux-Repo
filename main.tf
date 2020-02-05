75c7525f9250de9dc2166f559e65a2019d50efc0
Josue Vigil <joshvigil21@gmail.com>
2020-02-05 15:13:01 -0600

    updates Files

--- main.tf
+++ main.tf
@@ -1,2 +1,11 @@
-provider "azurerm {
+provider "azurerm" {
+    subscription_id = "6b634ddb-75da-4367-821e-dce95176e6e3"
+client_id	= "aa71c71c-5905-4112-bb10-b9cad379000d"
+client_secret	= "bf87a800-8db0-416d-81f2-98cf572f0fa2"	
+tenand_id	= "0c4fb07c-5d7f-4169-b8a5-867da4dfc340"
+    version = "=1.33.0"
+    }
+provider "random" {
+  
 }
+
